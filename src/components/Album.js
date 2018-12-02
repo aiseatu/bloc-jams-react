8ec48eab8a1f28c4ee98d3712aa05b78709a1d53
Xiaokang Tang <xiaokangt@hotmail.com>
2018-12-02 13:49:55 -0500

    assignment-2-3-8 complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -57,6 +57,15 @@ class Album extends Component{
     this.play();
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const trackLength = this.state.album.songs.length;
+    const newIndex = Math.min(trackLength - 1, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
   hover(song) {
     this.setState({ isHovering: true, hoveredSong: song });
   }
@@ -114,6 +123,7 @@ class Album extends Component{
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
